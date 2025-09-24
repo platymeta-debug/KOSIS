8751331e8ebea848137cacfb7e02518b441819ec
platymeta-debug <platymeta@gmail.com>
2025-09-24 18:10:26 +0900

    Delete src directory

--- src/model.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from statsmodels.tsa.api import VAR
-
-def fit_var(df: pd.DataFrame, lags=2):
-    return VAR(df.dropna()).fit(lags)
-
-def irf(res, periods=8):
-    return res.irf(periods)
